309e76f20be1f3e1b75116df848232054d59f0f5
Tronjan <workiniranjan10@gmail.com>
2023-03-13 22:54:40 +0530

    [UPDATE]

--- CvBuilderDev/Areas/Controllers/HeaderController.cs
+++ CvBuilderDev/Areas/Controllers/HeaderController.cs
@@ -1,10 +1,17 @@
 ﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
 namespace CvBuilderDev.Areas.Controllers
 {
-	public class HeaderController
+	public class HeaderController : Controller
 	{
 		public HeaderController()
 		{
+		}
+
+		public async Task<IActionResult> Get()
+		{
+			return Ok();
 		}
 	}
 }
